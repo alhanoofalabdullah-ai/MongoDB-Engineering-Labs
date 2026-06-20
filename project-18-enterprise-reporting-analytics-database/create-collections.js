64c4c0b195a574815bc5ee02d07697eeba3c6678
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-20 16:43:51 +0300

    Create create-collections.js

---
+++ project-18-enterprise-reporting-analytics-database/create-collections.js
@@ -0,0 +1,13 @@
+use enterprise_reporting_db;
+
+db.createCollection("kpis");
+
+db.createCollection("dashboards");
+
+db.createCollection("reports");
+
+db.createCollection("business_metrics");
+
+db.createCollection("report_schedules");
+
+db.createCollection("analytics_snapshots");
