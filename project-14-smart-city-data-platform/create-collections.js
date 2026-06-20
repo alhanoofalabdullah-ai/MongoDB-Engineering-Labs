384b7938e2e8d19b00064ab9d0ce346cad5fe2f4
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-20 16:13:19 +0300

    Create create-collections.js

---
+++ project-14-smart-city-data-platform/create-collections.js
@@ -0,0 +1,13 @@
+use enterprise_smartcity_db;
+
+db.createCollection("sensors");
+
+db.createCollection("traffic_data");
+
+db.createCollection("energy_consumption");
+
+db.createCollection("environmental_data");
+
+db.createCollection("public_services");
+
+db.createCollection("city_alerts");
