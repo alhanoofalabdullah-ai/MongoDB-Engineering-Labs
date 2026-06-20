87831dcbbea413e96ba4b8fab88fe00744a4f662
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-20 14:52:08 +0300

    Create create-collections.js

---
+++ project-11-ecommerce-backend-database/create-collections.js
@@ -0,0 +1,13 @@
+use enterprise_ecommerce_db;
+
+db.createCollection("customers");
+
+db.createCollection("products");
+
+db.createCollection("shopping_cart");
+
+db.createCollection("orders");
+
+db.createCollection("payments");
+
+db.createCollection("reviews");
