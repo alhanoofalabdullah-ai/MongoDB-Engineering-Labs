ad579de31286b630cd87b328edbcce5698be2d1a
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-20 14:22:52 +0300

    Create create-collections.js

---
+++ project-09-healthcare-records-system/create-collections.js
@@ -0,0 +1,13 @@
+use enterprise_healthcare_db;
+
+db.createCollection("patients");
+
+db.createCollection("doctors");
+
+db.createCollection("appointments");
+
+db.createCollection("prescriptions");
+
+db.createCollection("medical_history");
+
+db.createCollection("departments");
