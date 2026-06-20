228f55582b3fcd114a970e6911e30374aa258a40
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-20 14:09:09 +0300

    Create create-collections.js

---
+++ project-08-iot-device-monitoring-platform/create-collections.js
@@ -0,0 +1,11 @@
+use enterprise_iot_db;
+
+db.createCollection("devices");
+
+db.createCollection("sensors");
+
+db.createCollection("telemetry");
+
+db.createCollection("alerts");
+
+db.createCollection("device_health");
