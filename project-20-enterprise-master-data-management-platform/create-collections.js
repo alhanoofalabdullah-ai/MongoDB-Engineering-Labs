0c4c402bbabfae2de244672563f1fe99b16d7f7e
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-20 16:58:22 +0300

    Create create-collections.js

---
+++ project-20-enterprise-master-data-management-platform/create-collections.js
@@ -0,0 +1,15 @@
+use enterprise_mdm_db;
+
+db.createCollection("customer_master");
+
+db.createCollection("supplier_master");
+
+db.createCollection("product_master");
+
+db.createCollection("golden_records");
+
+db.createCollection("reference_data");
+
+db.createCollection("matching_rules");
+
+db.createCollection("data_quality_scores");
