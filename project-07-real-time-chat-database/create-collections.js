cf9416b5b0ca3038c220ad06b4fc9b173a3b422e
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-20 10:35:24 +0300

    Create create-collections.js

---
+++ project-07-real-time-chat-database/create-collections.js
@@ -0,0 +1,7 @@
+use enterprise_chat_db;
+
+db.createCollection("users");
+db.createCollection("chat_rooms");
+db.createCollection("messages");
+db.createCollection("user_sessions");
+db.createCollection("read_receipts");
