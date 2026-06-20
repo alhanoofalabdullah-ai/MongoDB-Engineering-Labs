d821e6bb00f89aeb3c3bb5106f14241373e0a7cb
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-20 14:44:04 +0300

    Create create-collections.js

---
+++ project-10-financial-transactions-database/create-collections.js
@@ -0,0 +1,13 @@
+use enterprise_financial_db;
+
+db.createCollection("customers");
+
+db.createCollection("accounts");
+
+db.createCollection("transactions");
+
+db.createCollection("payments");
+
+db.createCollection("audit_logs");
+
+db.createCollection("fraud_alerts");
