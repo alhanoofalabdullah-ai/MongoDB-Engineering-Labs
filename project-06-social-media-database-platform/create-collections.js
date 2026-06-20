9fc0d548855cf3e63022f54064e31fc6d8d8abbf
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-20 10:25:38 +0300

    Create create-collections.js

---
+++ project-06-social-media-database-platform/create-collections.js
@@ -0,0 +1,7 @@
+use enterprise_social_db;
+
+db.createCollection("users");
+db.createCollection("posts");
+db.createCollection("comments");
+db.createCollection("reactions");
+db.createCollection("followers");
