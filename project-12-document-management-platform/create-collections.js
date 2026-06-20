7236410da966477ff797978016c8f2ac0e9ce685
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-20 14:58:35 +0300

    Create create-collections.js

---
+++ project-12-document-management-platform/create-collections.js
@@ -0,0 +1,13 @@
+use enterprise_document_db;
+
+db.createCollection("documents");
+
+db.createCollection("document_versions");
+
+db.createCollection("workflows");
+
+db.createCollection("approvals");
+
+db.createCollection("users");
+
+db.createCollection("audit_logs");
