ec08bae4a01cf8e8990b8d7c6bd37ef156eabcf4
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-20 16:50:55 +0300

    Create create-collections.js

---
+++ project-19-enterprise-data-governance-platform/create-collections.js
@@ -0,0 +1,15 @@
+use enterprise_governance_db;
+
+db.createCollection("metadata_repository");
+
+db.createCollection("data_assets");
+
+db.createCollection("data_quality_rules");
+
+db.createCollection("data_lineage");
+
+db.createCollection("compliance_records");
+
+db.createCollection("master_data");
+
+db.createCollection("data_stewards");
