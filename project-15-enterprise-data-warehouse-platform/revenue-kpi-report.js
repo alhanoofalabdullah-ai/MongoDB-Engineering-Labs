0d3bb9244a3e766315de7785d49198c223060e69
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-20 16:23:00 +0300

    Create revenue-kpi-report.js

---
+++ project-15-enterprise-data-warehouse-platform/revenue-kpi-report.js
@@ -0,0 +1,15 @@
+use enterprise_data_warehouse_db;
+
+db.fact_sales.aggregate([
+{
+    $group: {
+        _id: null,
+        totalRevenue: {
+            $sum: "$revenue"
+        },
+        totalQuantity: {
+            $sum: "$quantity"
+        }
+    }
+}
+]);
